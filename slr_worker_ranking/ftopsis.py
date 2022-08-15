dfabbd665b6e8b4a9ec6dccbab4aad81f6ac9849
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2022-08-15 15:11:17 +0100

    Working in 1-2 step of fuzzy topsis

--- slr_worker_ranking/ftopsis.py
+++ slr_worker_ranking/ftopsis.py
@@ -16,6 +16,11 @@ class FuzzyTOPSIS(object):
         self.benefit_criteria = k_benefit_crit
         self.cost_criteria = k_cost_crit
         self.agg_alt_fuzzy_method = self._defaut_alt_agg_fuzzy_rating_method
+        self.agg_crit_fuzzy_method = self._defaut_crit_agg_fuzzy_weight_method
+
+        self.agg_alternatives = None
+        self.agg_benefit_criteria = None
+        self.agg_cost_criteria = None
 
 
     def validate_inputs(self, alt, k_benefit_crit, k_cost_crit):
@@ -34,30 +39,6 @@ class FuzzyTOPSIS(object):
         return alternatives_ranking
 
 
-    # def _assign_ratings(self):
-    #     """
-    #     First step in fuzzy TOPSIS, where ratings are assigned to all criteria and alternatives
-    #     """
-
-    #     for i in len(self.alternatives):
-    #         i_alt = self.alternatives[i]
-    #         for j in range(len(i_alt)):
-    #             i_j_crit = i_alt[j]
-    #             for k in range(len(i_j_crit)):
-    #                 # (9, 10, 10) -> Very Good
-    #                 i_j_k_fuzzy_rating = i_j_crit[k]
-    #                 pass
-
-    #     for k in range(len(self.alternatives)):
-    #         k_alternatives = self.alternatives[k]
-    #         for i in range(len(k_alternatives)):
-    #             i_alt = k_alternatives[i]
-    #             for j in range(len(i_alt)):
-    #                 k_i_j_crit = i_alt[j]
-    #             i_alt = [cj_k]
-    #             k_i_alt_fuzzy_ratings  
-
-
     def _defaut_alt_agg_fuzzy_rating_method(self, crit_alt):
         left_value, middle_value, right_value = crit_alt[0]
 
@@ -67,14 +48,14 @@ class FuzzyTOPSIS(object):
             left_value = min(left_value, crit_alt_k[0])
             middle_value += crit_alt_k[1]
             right_value = max(right_value, crit_alt_k[2])
+
         middle_value = middle_value / total_decision_makers
 
         return (left_value, middle_value, right_value)
 
-
     def _all_agg_ratings(self):
         """
-            Function used to aggregate the fuzzy ratings for the alternativas
+            Function used to aggregate the fuzzy ratings for the alternatives
         """
         agg_alt_ratings = []
         for i in range(len(self.alternatives)):
@@ -87,11 +68,47 @@ class FuzzyTOPSIS(object):
             agg_alt_ratings.append(agg_alt_i_ratings)
         return agg_alt_ratings
 
+    def _defaut_crit_agg_fuzzy_weight_method(self, crit_weights):
+
+        # for each decision maker K weight specified for this criteria (initialising with first K)
+        left_value, middle_value, right_value = crit_weights[0]
+
+        total_decision_makers = len(crit_weights)
+        for crit_w_k in crit_weights[1:]:
+            left_value = min(left_value, crit_w_k[0])
+            middle_value += crit_w_k[1]
+            right_value = max(right_value, crit_w_k[2])
+
+        middle_value = middle_value / total_decision_makers
+
+        return (left_value, middle_value, right_value)
+
+    def _all_agg_weights(self):
+        """
+            Function used to aggregate the fuzzy weights for the benefit and
+            cost criteria respectivelly
+        """
+        agg_benefit_crit = []
+        agg_cost_crit = []
+        for j in range(len(self.benefit_criteria)):
+            crit_weights = self.benefit_criteria[j]
+            aggr = self.agg_crit_fuzzy_method(crit_weights)
+            agg_benefit_crit.append(aggr)
+        for j in range(len(self.cost_criteria)):
+            crit_weights = self.cost_criteria[j]
+            aggr = self.agg_crit_fuzzy_method(crit_weights)
+            agg_cost_crit.append(aggr)
+
+        return agg_benefit_crit, agg_cost_crit
+
     def _aggregated_ratings_and_weights(self):
         """
         Second step in fuzzy TOPSIS, where the aggregated fuzzy ratings and weights are calculated 
         for the criteria and alternatives, respectivelly.
         """
+        self.agg_alternatives = self._all_agg_ratings()
+        self.agg_benefit_criteria, self.agg_cost_criteria = self._all_agg_weights()
+
 
     def _normalized_decision_matrix(self):
         """
--- tests/test_ftopsis.py
+++ tests/test_ftopsis.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 from slr_worker_ranking.ftopsis import FuzzyTOPSIS
 
@@ -62,3 +62,66 @@ class TestFuzzyTOPSIS(TestCase):
             exp_rating = exp_alt2_ratings[j]
             for triple_val_index in range(3):
                 self.assertAlmostEqual(exp_rating[triple_val_index], rating[triple_val_index], places=2)
+
+
+
+    def test_all_agg_weights(self):
+        high_weight = (0.7, 0.9, 1.0)
+        medium_high_weight = (0.5, 0.7, 0.9)
+        medium_weight = (0.3, 0.5, 0.7)
+        medium_low_weight = (0.1, 0.3, 0.5)
+        low_weight = (0.0, 0.1, 0.3)
+
+
+        criteria = ['c1', 'c2', 'c3']
+        decision_makers = ['k1', 'k2', 'k3']
+        self.ranker.benefit_criteria = [
+            [high_weight, medium_high_weight, high_weight], # (0.5, 0.833, 1.0)
+            [medium_weight, medium_low_weight, low_weight]  # (0.0, 0.3, 0.7)
+        ]
+        self.ranker.cost_criteria = [
+            [low_weight, medium_weight, medium_weight], # (0.0, 0.366, 0.7)
+            [low_weight, low_weight, low_weight] # (0.0, 0.1, 0.3)
+        ]
+
+
+        agg_benefit_crit, agg_cost_crit = self.ranker._all_agg_weights()
+
+
+        exp_agg_benefit_crit = [(0.5, 0.833, 1.0), (0.0, 0.3, 0.7)]
+        exp_agg_cost_crit = [(0.0, 0.366, 0.7), (0.0, 0.1, 0.3)]
+
+        self.assertEqual(len(agg_benefit_crit), 2)
+        self.assertEqual(len(agg_cost_crit), 2)
+
+        self.assertEqual(len(agg_benefit_crit[0]), 3)
+        self.assertEqual(len(exp_agg_cost_crit[0]), 3)
+
+
+        
+        for j in range(len(exp_agg_benefit_crit)):
+            crit_w = agg_benefit_crit[j]
+            exp_w = exp_agg_benefit_crit[j]
+            for triple_val_index in range(3):
+                self.assertAlmostEqual(exp_w[triple_val_index], crit_w[triple_val_index], places=2)
+
+        for j in range(len(exp_agg_cost_crit)):
+            crit_w = agg_cost_crit[j]
+            exp_w = exp_agg_cost_crit[j]
+            for triple_val_index in range(3):
+                self.assertAlmostEqual(exp_w[triple_val_index], crit_w[triple_val_index], places=2)
+
+    @patch('slr_worker_ranking.ftopsis.FuzzyTOPSIS._all_agg_ratings')
+    @patch('slr_worker_ranking.ftopsis.FuzzyTOPSIS._all_agg_weights')
+    def test_aggregated_ratings_and_weights_should_call_methods_and_set_vars(self, m_agg_w, m_agg_r):
+        agg_r = 'agg alternatives'
+        m_agg_r.return_value = agg_r
+        agg_w = ['Benefit crit weights', 'cost crit weights']
+        m_agg_w.return_value = agg_w
+
+        self.ranker._aggregated_ratings_and_weights()
+        m_agg_r.assert_called_once()
+        m_agg_w.assert_called_once()
+        self.assertEqual(self.ranker.agg_alternatives, agg_r)
+        self.assertEqual(self.ranker.agg_benefit_criteria, agg_w[0])
+        self.assertEqual(self.ranker.agg_cost_criteria, agg_w[1])
