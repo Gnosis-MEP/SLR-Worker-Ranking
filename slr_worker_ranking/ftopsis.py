d3632f3293530847f399477e705e6051452de9d4
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2022-08-22 19:38:08 +0100

    finishing refactor, way better now. and with tests

--- slr_worker_ranking/ftopsis.py
+++ slr_worker_ranking/ftopsis.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 class FuzzyTOPSIS(object):
     """
@@ -8,40 +10,68 @@ class FuzzyTOPSIS(object):
 
     Example of input:
     decision_maker_1_decision_matrix = [
-        [(9, 10, 10), (9, 10, 10), (9, 10, 10)] # alt1: crit1, 2 and 3 as "very_good_rating",
-        [(3, 5, 7), (3, 5, 7), (3, 5, 7)] # alt2: crit1, 2 and 3 as "medium_rating",
+        [(9, 10, 10), (9, 10, 10), (9, 10, 10)], # alt1: crit1, 2 and 3 as "very_good_rating"
+        [(3, 5, 7), (3, 5, 7), (3, 5, 7)], # alt2: crit1, 2 and 3 as "medium_rating"
     ]
     decision_maker_1_weights = [(0.7, 0.9, 1.0), (0.3, 0.5, 0.7), (0.3, 0.5, 0.7)] # weights for crit1, 2 and 3 as "high_weight", "medium_weight" the last two
 
     decision_maker_2_decision_matrix = [
-        [(3, 5, 7), (3, 5, 7), (3, 5, 7)] # alt1: crit1, 2 and 3 as "medium_rating",
-        [(7, 9, 10), (7, 9, 10), (7, 9, 10)] # alt2: crit1, 2 and 3 as "good_rating",
+        [(3, 5, 7), (3, 5, 7), (3, 5, 7)], # alt1: crit1, 2 and 3 as "medium_rating"
+        [(7, 9, 10), (7, 9, 10), (7, 9, 10)], # alt2: crit1, 2 and 3 as "good_rating"
     ]
     decision_maker_2_weights = [(0.3, 0.5, 0.7), (0.5, 0.7, 0.9), (0.5, 0.7, 0.9)] # weights for crit1, 2 and 3 as "medium_weight", "medium_high_weight" the last two
 
     decision_matrix_list = [decision_maker_1_decision_matrix, decision_maker_2_decision_matrix]
     weights_list = [decision_maker_1_weights, decision_maker_2_weights]
-    criteria_benefit_indicator = [True, False, True] # indicating that crit1 and 3 are benefit, and crit 2 is cost.
+    criteria_benefit_indicator = [True, False, True] # indicates that crit1 and 3 are benefit, and crit 2 is cost.
     """
 
 
-    def __init__(self, alt, k_benefit_crit, k_cost_crit):
-        self.validate_inputs(alt, k_benefit_crit, k_cost_crit)
+    def __init__(self, criteria_benefit_indicator):
+        self.criteria_benefit_indicator = criteria_benefit_indicator
+        self.num_alternatives = None
+        self.num_criteria = len(self.criteria_benefit_indicator)
+        self.decision_matrix_list = []
+        self.criteria_weights_list = []
 
-        self.alternatives = alt
-        self.benefit_criteria = k_benefit_crit
-        self.cost_criteria = k_cost_crit
         self.agg_alt_fuzzy_method = self._defaut_alt_agg_fuzzy_rating_method
         self.agg_crit_fuzzy_method = self._defaut_crit_agg_fuzzy_weight_method
+        self.norm_alt_fuzzy_method = self._default_normalize_alternative_method
 
-        self.agg_alternatives = None
-        self.agg_benefit_criteria = None
-        self.agg_cost_criteria = None
+        self.agg_decision_matrix = None
+        self.agg_criteria_weights = None
+
+        # self.validate_inputs(alt, k_benefit_crit, k_cost_crit)
+
+        # self.alternatives = alt
+        # self.benefit_criteria = k_benefit_crit
+        # self.cost_criteria = k_cost_crit
+
+        # self.agg_alternatives = None
+        # self.agg_benefit_criteria = None
+        # self.agg_cost_criteria = None
 
 
-    def validate_inputs(self, alt, k_benefit_crit, k_cost_crit):
-        assert len(k_benefit_crit) == len(k_cost_crit), "Inconsistent decision makers criteria vectors lenght."
-        assert len(k_benefit_crit) == len(alt), "Inconsistent decision makers alternatives vectors lenght."
+    # def validate_inputs(self, alt, k_benefit_crit, k_cost_crit):
+    #     assert len(k_benefit_crit) == len(k_cost_crit), "Inconsistent decision makers criteria vectors lenght."
+    #     assert len(k_benefit_crit) == len(alt), "Inconsistent decision makers alternatives vectors lenght."
+
+
+    def add_decision_maker(self, decision_matrix, criteria_weights):
+        num_alternatives = len(decision_matrix)
+        num_criteria = len(decision_matrix[0])
+        if self.num_alternatives is None:
+            self.num_alternatives = num_alternatives
+        else:
+            assert num_alternatives == self.num_alternatives, f"invalid number of alternatives in new decision matrix: {num_alternatives} != {self.num_alternatives}"
+
+        assert num_criteria == self.num_criteria, f"invalid number of criteria in new decision matrix: {num_criteria} != {self.num_criteria}"
+        num_criteria_w = len(criteria_weights)
+        assert num_criteria_w == self.num_criteria,  f"invalid number of criteria in new criteria weights: {num_criteria_w} != {self.num_criteria}"
+
+        self.decision_matrix_list.append(decision_matrix)
+        self.criteria_weights_list.append(criteria_weights)
+        self.num_decision_makers = len(self.decision_matrix_list)
 
     def evaluate(self):
         # self._assign_ratings_and_weights()
@@ -55,120 +85,105 @@ class FuzzyTOPSIS(object):
         return alternatives_ranking
 
 
-    def _defaut_alt_agg_fuzzy_rating_method(self, crit_alt):
-        left_value, middle_value, right_value = crit_alt[0]
+    def _defaut_alt_agg_fuzzy_rating_method(self, alt_i, crit_j):
+        min_left = np.inf
+        avg_middle = 0
+        max_right = 0
+        for dm in self.decision_matrix_list:
+            alternative = dm[alt_i]
+            criterion = alternative[crit_j]
+            c_left, c_middle, c_right = criterion
 
-        total_decision_makers = len(crit_alt)
-        # for each decision maker K rating specified for this criteria alternative (initialising with first K)
-        for crit_alt_k in crit_alt[1:]:
-            left_value = min(left_value, crit_alt_k[0])
-            middle_value += crit_alt_k[1]
-            right_value = max(right_value, crit_alt_k[2])
+            min_left = min(min_left, c_left)
+            avg_middle += c_middle
+            max_right = max(max_right, c_right)
+        avg_middle = avg_middle / self.num_decision_makers
 
-        middle_value = middle_value / total_decision_makers
-
-        return (left_value, middle_value, right_value)
+        agg_alt_i_crit_j = (min_left, avg_middle, max_right)
+        return agg_alt_i_crit_j
 
     def _all_agg_ratings(self):
         """
-            Function used to aggregate the fuzzy ratings for the alternatives
+            Function used to aggregate the fuzzy ratings for the alternatives for each decision maker
         """
-        agg_alt_ratings = []
-        for i in range(len(self.alternatives)):
-            agg_alt_i_ratings = []
-            i_alt_criterias = self.alternatives[i]
-            for j in range(len(i_alt_criterias)):
-                i_j_crit_alt = i_alt_criterias[j]
-                agg_rating = self.agg_alt_fuzzy_method(i_j_crit_alt)
-                agg_alt_i_ratings.append(agg_rating)
-            agg_alt_ratings.append(agg_alt_i_ratings)
-        return agg_alt_ratings
+        agg_decision_matrix = []
+        for alt_i in range(self.num_alternatives):
+            agg_alt_i_criteria = []
+            for crit_j in range(self.num_criteria):
+                agg_alt_i_crit_j = self.agg_alt_fuzzy_method(alt_i, crit_j)
+                agg_alt_i_criteria.append(agg_alt_i_crit_j)
 
-    def _defaut_crit_agg_fuzzy_weight_method(self, crit_weights):
+            agg_decision_matrix.append(agg_alt_i_criteria)
+        return agg_decision_matrix
 
+
+    def _defaut_crit_agg_fuzzy_weight_method(self, crit_j):
         # for each decision maker K weight specified for this criteria (initialising with first K)
-        left_value, middle_value, right_value = crit_weights[0]
+        min_left_value = np.inf
+        avg_middle_value = 0
+        max_right_value = 0
+        for weights in self.criteria_weights_list:
+            criterion_w = weights[crit_j]
 
-        total_decision_makers = len(crit_weights)
-        for crit_w_k in crit_weights[1:]:
-            left_value = min(left_value, crit_w_k[0])
-            middle_value += crit_w_k[1]
-            right_value = max(right_value, crit_w_k[2])
+            left_value, middle_value, right_value = criterion_w
+            min_left_value = min(min_left_value, left_value)
+            avg_middle_value += middle_value
+            max_right_value = max(max_right_value, right_value)
 
-        middle_value = middle_value / total_decision_makers
+        avg_middle_value = avg_middle_value / self.num_decision_makers
 
-        return (left_value, middle_value, right_value)
+        return (min_left_value, avg_middle_value, max_right_value)
 
     def _all_agg_weights(self):
         """
             Function used to aggregate the fuzzy weights for the benefit and
             cost criteria respectivelly
         """
-        agg_benefit_crit = []
-        agg_cost_crit = []
-        for j in range(len(self.benefit_criteria)):
-            crit_weights = self.benefit_criteria[j]
-            aggr = self.agg_crit_fuzzy_method(crit_weights)
-            agg_benefit_crit.append(aggr)
-        for j in range(len(self.cost_criteria)):
-            crit_weights = self.cost_criteria[j]
-            aggr = self.agg_crit_fuzzy_method(crit_weights)
-            agg_cost_crit.append(aggr)
 
-        return agg_benefit_crit, agg_cost_crit
+        agg_weights = []
+        for crit_j in range(self.num_criteria):
+            agg_crit_j = self.agg_crit_fuzzy_method(crit_j)
+            agg_weights.append(agg_crit_j)
+
+        return agg_weights
 
     def _aggregated_ratings_and_weights(self):
         """
         Second step in fuzzy TOPSIS, where the aggregated fuzzy ratings and weights are calculated
         for the criteria and alternatives, respectivelly.
         """
-        self.agg_alternatives = self._all_agg_ratings()
-        self.agg_benefit_criteria, self.agg_cost_criteria = self._all_agg_weights()
+        self.agg_decision_matrix = self._all_agg_ratings()
+        self.agg_criteria_weights = self._all_agg_weights()
 
 
-    # def _calculate_norm_alt_ratings_benefit_criteria(self):
-    #     def _get_tri_fuzz_right_value(fuzz_val):
-    #         return fuzz_val[2]
-    #     def _get_tri_fuzz_left_value(fuzz_val):
-    #         return fuzz_val[0]
-    #     norm_ratings_benefit_crit = []
+    def _get_min_left_or_max_right_for_criteria(self, crit_j):
+        is_benefit_criterion = self.criteria_benefit_indicator[crit_j]
+        final_value = 0
+        if is_benefit_criterion is False:
+            final_value = np.inf
+        # get min/max for cost/benefit criteria
+        for alternative in self.agg_decision_matrix:
+            criterion = alternative[crit_j]
+            left_value, middle_value, right_value = criterion
 
+            if is_benefit_criterion:
+                final_value = max(final_value, right_value)
+            else:
+                final_value = min(final_value, left_value)
+        return final_value
 
+    def _default_normalize_alternative_method(self, alt_i, crit_j, minl_or_maxr_criteria):
+        alternative = self.agg_decision_matrix[alt_i]
+        criterion = alternative[crit_j]
+        left_value, middle_value, right_value = criterion
 
-    #     max_right_value = max(
-    #         [self.agg_alternatives[i][crit_index] for i in range(len(self.agg_alternatives))],
-    #         key=_get_tri_fuzz_right_value
-    #     )[2]
-    #     norm_alt_ratings = []
-    #     for i in range(len(self.alternatives)):
-    #         norm_alt_i_ratings = []
-    #         i_alt_criterias = self.alternatives[i]
-
-    #         for j in range(len(i_alt_criterias)):
-    #             i_j_crit_alt = i_alt_criterias[j]
-
-
-    #     for i in range(len(self.agg_alternatives)):
-
-    #         for j in range(len(self.benefit_criteria)):
-    #         crit_index = j
-    #         # max_right_value = self.agg_alternatives[0][crit_index]
-    #         max_right_value = max(
-    #             [self.agg_alternatives[i][crit_index] for i in range(len(self.agg_alternatives))],
-    #             key=_get_tri_fuzz_right_value
-    #         )[2]
-    #         benefit_crit_max = (max_right_value, max_right_value, max_right_value)
-    #             agg_alt_i_j_rating = self.agg_alternatives[i][crit_index]
-    #             norm_rating = []
-    #             for v in range(3):
-    #                 norm_rating.append(self.agg_alternatives[i][crit_index][v] / benefit_crit_max[v])
-    #             norm_ratings_benefit_crit.append(norm_rating)
-
-
-    #         # min_left_value = min(
-    #         #     [self.agg_alternatives[i][crit_index] for i in range(len(self.agg_alternatives))],
-    #         #     key=_get_tri_fuzz_left_value
-    #         # )[2]
+        norm_alt_crit_j = None
+        is_benefit_criterion = self.criteria_benefit_indicator[crit_j]
+        if is_benefit_criterion:
+            norm_alt_crit_j = ((left_value / minl_or_maxr_criteria), (middle_value / minl_or_maxr_criteria), (right_value / minl_or_maxr_criteria))
+        else:
+            norm_alt_crit_j = ((minl_or_maxr_criteria / right_value), (minl_or_maxr_criteria / middle_value), (minl_or_maxr_criteria / left_value))
+        return norm_alt_crit_j
 
 
     def _normalized_decision_matrix(self):
@@ -176,7 +191,15 @@ class FuzzyTOPSIS(object):
         Third step in fuzzy TOPSIS, in which the normalized fuzzy decision matrix is calculated.
         """
 
+        norm_decision_matrix = [[None for j in range(self.num_criteria)] for i in range(self.num_alternatives)]
+        for crit_j in range(self.num_criteria):
+            minl_or_maxr_criteria = self._get_min_left_or_max_right_for_criteria(crit_j)
 
+            for alt_i in range(self.num_alternatives):
+                norm_alt_crit_j = self.norm_alt_fuzzy_method(alt_i, crit_j, minl_or_maxr_criteria)
+                norm_decision_matrix[alt_i][crit_j] = norm_alt_crit_j
+
+        return norm_decision_matrix
 
     def _weighted_normalized_decision_matrix(self):
         """
--- tests/test_ftopsis.py
+++ tests/test_ftopsis.py
@@ -5,123 +5,165 @@ from slr_worker_ranking.ftopsis import FuzzyTOPSIS
 
 
 class TestFuzzyTOPSIS(TestCase):
-    
+
     def setUp(self):
-        self.ranker = FuzzyTOPSIS(alt=[], k_benefit_crit=[], k_cost_crit=[])
+        self.rat_lf = {
+            'very_good_rating': (9, 10, 10),
+            'good_rating': (7, 9, 10),
+            'medium_rating': (3, 5, 7),
+            'poor_rating': (1, 3, 5),
+            'very_poor_rating': (1, 1, 3)
+        }
+        self.crit_lf = {
+            'high_weight': (0.7, 0.9, 1.0),
+            'medium_high_weight': (0.5, 0.7, 0.9),
+            'medium_weight': (0.3, 0.5, 0.7),
+            'medium_low_weight': (0.1, 0.3, 0.5),
+            'low_weight': (0.0, 0.1, 0.3),
+        }
+
+
+        self.dm_1 = {
+            'decision_matrix': [
+                [self.rat_lf['very_good_rating'], self.rat_lf['medium_rating'], self.rat_lf['poor_rating']],
+                [self.rat_lf['medium_rating'], self.rat_lf['poor_rating'], self.rat_lf['medium_rating']]
+            ],
+            'criteria_weights': [ # weights for crit1, 2 and 3 as "high_weight", "medium_weight" the last two
+                self.crit_lf['high_weight'], self.crit_lf['medium_weight'], self.crit_lf['medium_weight']
+            ]
+        }
+
+        self.dm_2 = {
+            'decision_matrix': [
+                [self.rat_lf['medium_rating'], self.rat_lf['good_rating'], self.rat_lf['medium_rating']],
+                [self.rat_lf['good_rating'], self.rat_lf['poor_rating'], self.rat_lf['very_good_rating']]
+            ],
+            'criteria_weights': [
+                self.crit_lf['medium_weight'], self.crit_lf['medium_high_weight'], self.crit_lf['medium_high_weight']
+            ]
+        }
+        self.criteria_benefit_indicator = [True, False, True] # crit1 and 3 are benefit, and crit 2 is cost
+        self.ranker = FuzzyTOPSIS(criteria_benefit_indicator=self.criteria_benefit_indicator)
+        self.ranker.add_decision_maker(**self.dm_1)
+        self.ranker.add_decision_maker(**self.dm_2)
+
+    def test_add_decision_maker(self):
+        new_ranker = FuzzyTOPSIS(criteria_benefit_indicator=self.criteria_benefit_indicator)
+        new_ranker.add_decision_maker(**self.dm_1)
+
+        self.assertListEqual(new_ranker.decision_matrix_list, [self.dm_1['decision_matrix']])
+        self.assertListEqual(new_ranker.criteria_weights_list, [self.dm_1['criteria_weights']])
+
+        new_ranker.add_decision_maker(**self.dm_2)
+        self.assertListEqual(new_ranker.decision_matrix_list, [self.dm_1['decision_matrix'], self.dm_2['decision_matrix']])
+        self.assertListEqual(new_ranker.criteria_weights_list, [self.dm_1['criteria_weights'], self.dm_2['criteria_weights']])
+
+
+    def test_defaut_alt_agg_fuzzy_rating_method(self):
+        agg_alt_i_crit_j = self.ranker._defaut_alt_agg_fuzzy_rating_method(alt_i=0, crit_j=0)
+        exp_agg_alt_i_crit_j = (3, 7.5, 10) # min(a) avg(b) max(c) of very_good_rating and medium_rating
+        self.assertEqual(agg_alt_i_crit_j, exp_agg_alt_i_crit_j)
 
     def test_all_agg_ratings(self):
-        very_good_rating = (9, 10, 10)
-        good_rating = (7, 9, 10)
-        medium_rating = (3, 5, 7)
-        poor_rating = (1, 3, 5)
-        very_poor_rating = (1, 1, 3)
-
-        criteria = ['c1', 'c2', 'c3']
-        decision_makers = ['k1', 'k2', 'k3']
-        alt1 = [
-            [ very_good_rating, medium_rating, good_rating] 
-            for c in criteria
-        ]
-
-        alt2 = [
-            [ medium_rating, medium_rating, very_poor_rating] 
-            for c in criteria
-        ]
-
-        self.ranker.alternatives = [
-            alt1, 
-            alt2
-        ]
-        all_agg_ratings = self.ranker._all_agg_ratings()
-
-
-        exp_alt1_ratings = [(3, 8.0, 10), (3, 8.0, 10), (3, 8.0, 10)]
-        exp_alt2_ratings = [(1, 3.666, 7), (1, 3.666, 7), (1, 3.666, 7)]
+        agg_decision_matrix = self.ranker._all_agg_ratings()
+        exp_alt1_ratings = [(3, 7.5, 10), (3, 7, 10), (1, 4, 7)]
+        exp_alt2_ratings = [(3, 7, 10), (1, 3, 5), (3, 7.5, 10)]
         expected_ratings = [
             exp_alt1_ratings,
             exp_alt2_ratings
         ]
-        self.assertEqual(len(all_agg_ratings), 2)
+        self.assertEqual(len(agg_decision_matrix), 2)
 
-        self.assertEqual(len(all_agg_ratings[0]), 3)
-        self.assertEqual(len(all_agg_ratings[1]), 3)
+        self.assertEqual(len(agg_decision_matrix[0]), 3)
+        self.assertEqual(len(agg_decision_matrix[1]), 3)
 
-        self.assertEqual(len(all_agg_ratings[0][0]), 3)
+        self.assertEqual(len(agg_decision_matrix[0][0]), 3)
 
-        
-        for j in range(len(exp_alt1_ratings)):
-            alt_1_ratings = all_agg_ratings[0]
-            rating = alt_1_ratings[j]
-            exp_rating = exp_alt1_ratings[j]
-            for triple_val_index in range(3):
-                self.assertAlmostEqual(exp_rating[triple_val_index], rating[triple_val_index], places=2)
+        self.assertListEqual(agg_decision_matrix, expected_ratings)
 
-        for j in range(len(exp_alt2_ratings)):
-            alt_2_ratings = all_agg_ratings[1]
-            rating = alt_2_ratings[j]
-            exp_rating = exp_alt2_ratings[j]
-            for triple_val_index in range(3):
-                self.assertAlmostEqual(exp_rating[triple_val_index], rating[triple_val_index], places=2)
+    def test_defaut_crit_agg_fuzzy_weight_method(self):
+        agg_crit_j_w = self.ranker._defaut_crit_agg_fuzzy_weight_method(crit_j=0)
 
-
+        exp_agg_crit_j_w = (0.3, 0.7, 1.0) # min(a) avg(b) max(c) of high_weight and medium_weight
+        self.assertEqual(agg_crit_j_w, exp_agg_crit_j_w)
 
     def test_all_agg_weights(self):
-        high_weight = (0.7, 0.9, 1.0)
-        medium_high_weight = (0.5, 0.7, 0.9)
-        medium_weight = (0.3, 0.5, 0.7)
-        medium_low_weight = (0.1, 0.3, 0.5)
-        low_weight = (0.0, 0.1, 0.3)
+        agg_criteria_weights = self.ranker._all_agg_weights()
+        exp_agg_crit_weights = [(0.3, 0.7, 1.0), (0.3, 0.6, 0.9), (0.3, 0.6, 0.9)]
 
+        self.assertEqual(len(agg_criteria_weights), 3)
+        self.assertEqual(len(agg_criteria_weights[0]), 3)
+        self.assertEqual(len(agg_criteria_weights[1]), 3)
+        self.assertEqual(len(agg_criteria_weights[2]), 3)
 
-        criteria = ['c1', 'c2', 'c3']
-        decision_makers = ['k1', 'k2', 'k3']
-        self.ranker.benefit_criteria = [
-            [high_weight, medium_high_weight, high_weight], # (0.5, 0.833, 1.0)
-            [medium_weight, medium_low_weight, low_weight]  # (0.0, 0.3, 0.7)
-        ]
-        self.ranker.cost_criteria = [
-            [low_weight, medium_weight, medium_weight], # (0.0, 0.366, 0.7)
-            [low_weight, low_weight, low_weight] # (0.0, 0.1, 0.3)
-        ]
-
-
-        agg_benefit_crit, agg_cost_crit = self.ranker._all_agg_weights()
-
-
-        exp_agg_benefit_crit = [(0.5, 0.833, 1.0), (0.0, 0.3, 0.7)]
-        exp_agg_cost_crit = [(0.0, 0.366, 0.7), (0.0, 0.1, 0.3)]
-
-        self.assertEqual(len(agg_benefit_crit), 2)
-        self.assertEqual(len(agg_cost_crit), 2)
-
-        self.assertEqual(len(agg_benefit_crit[0]), 3)
-        self.assertEqual(len(exp_agg_cost_crit[0]), 3)
-
-
-        
-        for j in range(len(exp_agg_benefit_crit)):
-            crit_w = agg_benefit_crit[j]
-            exp_w = exp_agg_benefit_crit[j]
-            for triple_val_index in range(3):
-                self.assertAlmostEqual(exp_w[triple_val_index], crit_w[triple_val_index], places=2)
-
-        for j in range(len(exp_agg_cost_crit)):
-            crit_w = agg_cost_crit[j]
-            exp_w = exp_agg_cost_crit[j]
-            for triple_val_index in range(3):
-                self.assertAlmostEqual(exp_w[triple_val_index], crit_w[triple_val_index], places=2)
+        self.assertListEqual(agg_criteria_weights, exp_agg_crit_weights)
 
     @patch('slr_worker_ranking.ftopsis.FuzzyTOPSIS._all_agg_ratings')
     @patch('slr_worker_ranking.ftopsis.FuzzyTOPSIS._all_agg_weights')
     def test_aggregated_ratings_and_weights_should_call_methods_and_set_vars(self, m_agg_w, m_agg_r):
-        agg_r = 'agg alternatives'
+        agg_r = 'mock agg alternatives'
         m_agg_r.return_value = agg_r
-        agg_w = ['Benefit crit weights', 'cost crit weights']
+        agg_w = 'mock crit weights'
         m_agg_w.return_value = agg_w
 
         self.ranker._aggregated_ratings_and_weights()
         m_agg_r.assert_called_once()
         m_agg_w.assert_called_once()
-        self.assertEqual(self.ranker.agg_alternatives, agg_r)
-        self.assertEqual(self.ranker.agg_benefit_criteria, agg_w[0])
-        self.assertEqual(self.ranker.agg_cost_criteria, agg_w[1])
+        self.assertEqual(self.ranker.agg_decision_matrix, agg_r)
+        self.assertEqual(self.ranker.agg_criteria_weights, agg_w)
+
+
+    def test_get_min_left_or_max_right_for_criteria_for_benefit_criteria(self):
+        self.ranker.agg_decision_matrix = [
+            [(3, 7.5, 10), (3, 7, 10), (1, 4, 7)], # alt 1
+            [(3, 7, 10), (1, 3, 5), (3, 7.5, 10)] # alt 2
+        ]
+        minl_or_maxr_criteria = self.ranker._get_min_left_or_max_right_for_criteria(crit_j=0)
+        exp_value = 10
+        self.assertEqual(minl_or_maxr_criteria, exp_value)
+
+        minl_or_maxr_criteria = self.ranker._get_min_left_or_max_right_for_criteria(crit_j=2)
+        exp_value = 10
+        self.assertEqual(minl_or_maxr_criteria, exp_value)
+
+    def test_get_min_left_or_max_right_for_criteria_for_cost_criteria(self):
+        self.ranker.agg_decision_matrix = [
+            [(3, 7.5, 10), (3, 7, 10), (1, 4, 7)], # alt 1
+            [(3, 7, 10), (1, 3, 5), (3, 7.5, 10)] # alt 2
+        ]
+        minl_or_maxr_criteria = self.ranker._get_min_left_or_max_right_for_criteria(crit_j=1)
+        exp_value = 1
+        self.assertEqual(minl_or_maxr_criteria, exp_value)
+
+    def test_default_normalize_alternative_method_for_benefit_criteria(self):
+        self.ranker.agg_decision_matrix = [
+            [(3, 7.5, 10), (3, 7, 10), (1, 4, 7)], # alt 1
+            [(3, 7, 10), (1, 3, 5), (3, 7.5, 10)] # alt 2
+        ]
+        norm_alt_i_crit_j = self.ranker._default_normalize_alternative_method(alt_i=0, crit_j=0, minl_or_maxr_criteria=10)
+
+        exp_norm_alt_i_crit_j = (0.3, 0.75, 1.0)
+        self.assertEqual(norm_alt_i_crit_j, exp_norm_alt_i_crit_j)
+
+    def test_default_normalize_alternative_method_for_cost_criteria(self):
+        self.ranker.agg_decision_matrix = [
+            [(3, 7.5, 10), (3, 7, 10), (1, 4, 7)], # alt 1
+            [(3, 7, 10), (1, 3, 5), (3, 7.5, 10)] # alt 2
+        ]
+        norm_alt_i_crit_j = self.ranker._default_normalize_alternative_method(alt_i=0, crit_j=1, minl_or_maxr_criteria=1)
+
+        exp_norm_alt_i_crit_j = (1/10, 1/7, 1/3)
+        for v in range(len(exp_norm_alt_i_crit_j)):
+            self.assertAlmostEqual(norm_alt_i_crit_j[v], exp_norm_alt_i_crit_j[v], places=2)
+
+    def test_normalized_decision_matrix(self):
+        self.ranker.agg_decision_matrix = [
+            [(3, 7.5, 10), (3, 7, 10), (1, 4, 7)], # alt 1
+            [(3, 7, 10), (1, 3, 5), (3, 7.5, 10)] # alt 2
+        ]
+        norm_decision_matrix = self.ranker._normalized_decision_matrix()
+        exp_norm_decision_matrix = [
+            [(0.3, 0.75, 1.0), (1/10, 1/7, 1/3), (0.1, 0.4, 0.7)], # alt1
+            [(0.3, 0.7, 1.0), (1/5, 1/3, 1.0), (0.3, 0.75, 1)], # alt2
+        ]
+        self.assertEqual(norm_decision_matrix, exp_norm_decision_matrix)
